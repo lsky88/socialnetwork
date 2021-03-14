97316cc7dd4fde956a2723d2301d497de6b49f4a
lsky88 <l-sky@L-Sky.local>
2021-03-14 20:07:07 +0500

    51 Users API
    connected Axios
    added user image

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import css from './Users.module.css';
+import * as axios from 'axios';
+import userPhoto from '../../assets/img/user.jpg'
 
 let Users = (props) => {
   if (props.users.length === 0) {
-    props.setUsers({
-      users: [
-        {
-          id: 1,
-          followed: true,
-          photo: '',
-          fullName: 'Igor',
-          status: "i'm a boss!",
-          location: { country: 'USA', city: 'San-Francisco' },
-        },
-        {
-          id: 2,
-          followed: false,
-          photo: '',
-          fullName: 'Alfia',
-          status: "i'm a boss's wife!",
-          location: { country: 'USA', city: 'San-Francisco' },
-        },
-      ],
-    });
+    axios
+      .get('https://social-network.samuraijs.com/api/1.0/users​')
+      .then((response) => {
+        props.setUsers(response.data.items);
+      });
   }
 
   return (
@@ -30,31 +17,31 @@ let Users = (props) => {
       {props.users.map((user) => (
         <div className={css.user} key={user.id}>
           <div>
-            <img src="" alt="ava" />
+            <img src={userPhoto} alt="ava" />
           </div>
           <div className={css.userInfo}>
-          <div>{user.fullName}</div>
-          <div>{user.location.city},</div>
-          <div>{user.location.country}</div>
+            <div>{user.fullName}</div>
+            <div>{user.location.city},</div>
+            <div>{user.location.country}</div>
           </div>
           <div>{user.status}</div>
           {user.followed ? (
-              <button
-                onClick={() => {
-                  props.unfollow(user.id);
-                }}
-              >
-                Follow
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  props.follow(user.id);
-                }}
-              >
-                Unfollow
-              </button>
-            )}
+            <button
+              onClick={() => {
+                props.unfollow(user.id);
+              }}
+            >
+              Follow
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                props.follow(user.id);
+              }}
+            >
+              Unfollow
+            </button>
+          )}
         </div>
       ))}
     </div>
