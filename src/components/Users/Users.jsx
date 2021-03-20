ae7c7c9d412f6c11be0ac77d2507d5d39524d5d8
lsky88 <l-sky@L-Sky.local>
2021-03-21 00:38:46 +0500

    54 Life cycle
    componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,8 +3,7 @@ import css from './Users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../assets/img/user.jpg';
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
       .get('https://social-network.samuraijs.com/api/1.0/users')
       .then((response) => {
